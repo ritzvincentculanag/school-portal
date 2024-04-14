71871f215c5c5c590f2ad2b8f2abb817ba76c1c6
Ritchie <ritchieculanag@gmail.com>
2024-04-15 00:55:04 +0800

    Add initial urls

--- portal_dcph/urls.py
+++ portal_dcph/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("students/", include("students.urls")),
+    path("teachers/", include("teachers.urls")),
+    path("casheirs/", include("cashiers.urls")),
+    path("staffs/", include("staffs.urls"))
 ]
